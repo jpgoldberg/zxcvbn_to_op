5d4ad3a4af27d0ba6f059f009903de918d1b7f4f
Jeffrey Goldberg <jeff@agilebits.com>
2019-10-09 13:54:18 -0500

    Seems to work

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
 extern crate float_cmp;
 extern crate zxcvbn;
 
-use zxcvbn::{zxcvbn, ZxcvbnError};
-
 use float_cmp::ApproxEqRatio;
 
 use std::fmt;
-use std::io;
-use std::io::prelude::*;
-
-fn old_main() {
-    let mut pwd = String::new();
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        pwd = line.unwrap();
-        let g = zxcvbn_bits(pwd.clone()).unwrap();
-        println!("{}\t{}", pwd, g);
-    }
-}
 
 fn main() {
     // control points determined by eyeballing OP vs ZXCVBN scatter plot
@@ -34,34 +20,23 @@ fn main() {
             op: 100.0,
         },
     ];
-    let foo = mapping_from_control_points(points).unwrap();
+
+    lines_from_points(&points); // just print out what our functions are
+
+    let a_function = mapping_from_control_points(&points).unwrap();
 
     // should really be doing this in test functions
-    for z in &[-1.0, 0.0, 0.3, 2.0, 3.5, 4.2, 5.5, 6.2, 8.4, 9.0,
-                11.0, 12.0, 13.6, 15.0, 16.0, 17.0, 18.1, 19.0, 19.9, 21.0] {
-        println!("f({}) = {}", z, foo(*z as f32));
+    for z in &[
+        -1.0, 0.0, 0.3, 2.0, 3.5, 4.2, 5.5, 6.2, 8.4, 9.0, 11.0, 12.0, 13.6, 15.0, 16.0, 17.0,
+        18.1, 19.0, 19.9, 21.0,
+    ] {
+        println!("Via mapping: f({}) = {}", z, a_function(*z as f32));
+        println!(
+            "Via compute: f({}) = {}",
+            z,
+            compute_op_from_zxcvbn(*z, &points).unwrap()
+        );
     }
-}
-
-#[derive(Debug)]
-struct GuessEnt(u64, f64, u32);
-
-impl fmt::Display for GuessEnt {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // write!(f, "Guesses: {}, Bits: {}", self.0, self.2)
-        write!(f, "{}", self.1)
-    }
-}
-
-fn zxcvbn_bits(input: String) -> Result<GuessEnt, ZxcvbnError> {
-    let estimate = zxcvbn(&input, &[])?;
-    let guessing_bits = (estimate.guesses as f32).log(2.0) as u32 + 1;
-
-    Ok(GuessEnt(
-        estimate.guesses,
-        estimate.guesses_log10,
-        guessing_bits,
-    ))
 }
 
 // just need to create the mapping function from points
@@ -97,8 +72,7 @@ fn line_from_points(p1: &Point, p2: &Point) -> Option<Box<dyn Fn(f32) -> f32>> {
     Some(Box::new(move |x| x * m + b))
 }
 
-/// This assumes that the input is already sorted properly. Might add sorting later
-fn mapping_from_control_points(points: Vec<Point>) -> Option<Box<dyn Fn(f32) -> f32>> {
+fn compute_op_from_zxcvbn(op: f32, points: &Vec<Point>) -> Option<f32> {
     // We need to create a sequence of linear functions based on pairs of points
 
     if points.len() < 2 {
@@ -109,7 +83,6 @@ fn mapping_from_control_points(points: Vec<Point>) -> Option<Box<dyn Fn(f32) -> 
     // we have for that range
 
     struct Segment {
-        lower: f32,
         upper: f32,
         line_function: Box<dyn Fn(f32) -> f32>,
     };
@@ -125,7 +98,49 @@ fn mapping_from_control_points(points: Vec<Point>) -> Option<Box<dyn Fn(f32) -> 
         let first = &points[i - 1];
         let second = &points[i];
         segments.push(Segment {
-            lower: first.zx,
+            upper: second.zx,
+            line_function: line_from_points(first, second)?,
+        });
+    }
+
+    // segments has all of the information I need to build the actual function that we return
+    // if creating it manually, and knowing how many segments there were, we'd use a match
+    // construction. There probably is a clever way to do that, but let's not be so clever
+    for s in &segments {
+        if op < s.upper {
+            return Some((s.line_function)(op));
+        }
+    }
+    Some(100.0)
+}
+
+/// This assumes that the input is already sorted properly. Might add sorting later
+fn mapping_from_control_points(points: &Vec<Point>) -> Option<Box<dyn Fn(f32) -> f32>> {
+    // We need to create a sequence of linear functions based on pairs of points
+
+    if points.len() < 2 {
+        return None;
+    }
+
+    // Internally, we need to keep a list of range endpoints and the function
+    // we have for that range
+
+    struct Segment {
+        upper: f32,
+        line_function: Box<dyn Fn(f32) -> f32>,
+    };
+
+    let mut segments: Vec<Segment> = Vec::new();
+    segments.reserve_exact(points.len() - 1);
+
+    // this looping could be done more nicely with iter and nth(). But
+    // I'm old and don't know these new fangled things that kids use today.
+    //
+    // Deliberately starts at 1, as we look at element and the _previous_ element
+    for i in 1..points.len() {
+        let first = &points[i - 1];
+        let second = &points[i];
+        segments.push(Segment {
             upper: second.zx,
             line_function: line_from_points(first, second)?,
         });
@@ -139,20 +154,19 @@ fn mapping_from_control_points(points: Vec<Point>) -> Option<Box<dyn Fn(f32) -> 
         let mut ret = 100.0;
         for s in &segments {
             if x < s.upper {
-               ret = (s.line_function)(x);
+                ret = (s.line_function)(x);
+                break;
             }
         }
         ret
     }))
 }
 
-fn lines_from_points(points: Vec<Point>) -> Option<String> {
+fn lines_from_points(points: &Vec<Point>) {
     // assumes that points are already sorted
     if points.len() < 2 {
-        return None;
+        return;
     }
-
-    let out = String::from("string to keep IDE happy while I'm working");
 
     // we loop through from the second member, and we need an index.
     // Probably could use nth() but that requires reading docs
@@ -172,6 +186,4 @@ fn lines_from_points(points: Vec<Point>) -> Option<String> {
         println!("\nFor points {} and {}", first, second);
         println!("\ty = {}x + {}", m, b);
     }
-
-    Some(out)
 }
